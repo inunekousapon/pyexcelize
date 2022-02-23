e65fd039846e93bbfd1fa293dca4500ddc8ef393
JunichiYukawa <yukawa@ohmae.ac.jp>
2022-02-23 14:53:27 +0900

    Add Performance test

--- tests/test_pyexcelize.py
+++ tests/test_pyexcelize.py
@@ -1,8 +1,16 @@
 import unittest
 import os
 import shutil
+import resource
+from os.path import getsize
+from datetime import datetime
 
 import pyexcelize as pe
+
+
+def get_maxrss() -> float:
+    r = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    return r // 1024 // 1024  # bytes on MacOS
 
 
 class ASeriesOfTest(unittest.TestCase):
@@ -45,6 +53,34 @@ class ASeriesOfTest(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree('__tmp')
+
+
+class PerformanceTest(unittest.TestCase):
+
+    def test_performance(self):
+        print(f"{datetime.now().isoformat()} init: {get_maxrss()}MB")
+        index = pe.new_file()
+
+        txt = "1234567890" * 10
+
+        writer_index = pe.new_stream_writer(index, "Sheet1")
+        for row in range(1,50000):
+            params = []
+            for col in "ABCDEFGHIJKLMNOPQRST":
+                params.append(txt)
+            pe.set_row(writer_index, f"A{row}", params)
+
+        pe.flush(writer_index)
+
+        print(f"{datetime.now().isoformat()} writed: {get_maxrss()}MB")
+
+        pe.save_as(index, 'output.xlsx')
+
+        print(f"{datetime.now().isoformat()} saved: {get_maxrss()}MB")
+
+        pe.close(index)
+
+        print(f"{datetime.now().isoformat()} closed: {get_maxrss()}MB")
 
 
 if __name__ == '__main__':
